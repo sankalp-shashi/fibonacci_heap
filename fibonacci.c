8002c6bb13de32b020a08b23f84617512f6e08b0
sankalp <2022csb1137@iitrpr.ac.in>
2023-10-05 12:11:45 +0530

    First commit, Node structure and getnode() function.

---
+++ fibonacci.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+int max_degree = 3;
+
+
+typedef struct NODE
+{
+	int key;
+	int marked;
+	int degree;
+	struct NODE *left;
+	struct NODE *right;
+	struct NODE *children;
+	struct NODE *parent;
+}node; //typedef, to avoid typing 'struct' everytime.
+
+
+//Intended use: rootlist_end->right = getnode(rootlist_end);
+//		rootlist_end = rootlist_end->right;
+node *getnode(node *rootlist_end)
+{
+	node *newnode = (node*)malloc(sizeof(node)); //Creating a new node.
+	newnode->left = rootlist_end; //Initializing the pointers, and degree.
+	newnode->right = NULL;
+	newnode->parent = NULL;
+	newnode->degree = 0;
+	newnode->marked = 0;
+	newnode->children = NULL; //No children yet, will add space for them if needed.
+	return newnode;
+}
+
+
+
